69ab214df8f399d7c9817a072a18bb0040bbe76a
Maximilian Erfurt <maximilian.erfurt@gmail.com>
2023-04-14 14:25:33 +0200

    Bisl Überarbeitet

--- LearningEnvironment.py
+++ LearningEnvironment.py
@@ -3,13 +3,38 @@ import matplotlib.pyplot as plot
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-
+import scipy
 
 
 def create_rand_image():
-    array = np.zeros((400, 400), dtype=int, )
-    print(array)
-    image = Image.fromarray(array, "1")
-    image.show()
+    # image
+    x_size = 800
+    y_size = 400
+    array = np.zeros((y_size, x_size), dtype=int, )
+    # generating random points
+    x = np.arange(0, x_size + 10, x_size / 10, int)
+    print(len(x))
+    y = np.random.randint(0, y_size / 2, len(x))
+    print(len(y))
+    # generate cubic spline
+    spline = scipy.interpolate.CubicSpline(x=x, y=y)
+
+    xs = np.arange(0, x_size, 1)
+    fig, ax = plt.subplots(figsize=(6.5, 4))
+    ax.plot(x, y, 'o', label='data')
+    ax.plot(xs, spline(xs), label="S")
+    plt.show()
+
+    # merch into image
+    for i in range(x_size):
+        print(i)
+        print(int(spline(i).round()))
+        array[int(spline(i).round())][i] = 255
+        print(array[int(spline(i).round())][i])
+
+    img = Image.fromarray(array, '1')
+    img.save('C:/Images/img.png')
+    img.show()
+
 
 create_rand_image()
