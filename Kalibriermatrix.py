dbb301061e49cd6f9827909629cb754e7147d425
Maximilian Erfurt <maximilian.erfurt@gmail.com>
2023-05-15 13:15:33 +0200

    Kalibriermatrix berechnung fast fertig Bilder

---
+++ Kalibriermatrix.py
@@ -0,0 +1,57 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from datetime import datetime
+import tkinter
+from tkinter import filedialog
+from skimage.morphology import skeletonize
+
+# global var
+pixel_cords = np.ones((3, 4))
+real_cords = np.ones((4, 4))
+xi, yi = 0, 0
+
+
+# function to get the pixel cords
+# on mouseclick
+def click_event(event, x, y, flags, params):
+    global xi
+    global yi
+    # checking for event
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xi = x
+        yi = y
+
+
+# reading the image
+img = cv2.imread('KalibrierDraht.jpeg', 1)
+
+# for loop to get 4 pixel cords
+for i in range(4):
+    # setting mouse handler for the image
+    # and calling the click_event() function
+    cv2.imshow('image', img)
+    cv2.setMouseCallback('image', click_event)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # save cords in 4x2 matrix
+    pixel_cords[0, i] = xi
+    pixel_cords[1, i] = yi
+
+print(pixel_cords)
+
+# ask for real cords
+for i in range(4):
+    print("Point" + str(i+1) + " x :")
+    real_cords[0, i] = input()
+    print("Point" + str(i + 1) + " y :")
+    real_cords[1, i] = input()
+    print("Point" + str(i + 1) + " z :")
+    real_cords[2, i] = input()
+print(real_cords)
+
+k = np.matmul(np.linalg.inv(real_cords), pixel_cords)
+print(k)
+
+
