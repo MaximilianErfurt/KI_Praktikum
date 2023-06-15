60f7cef69f7fc622d2b5646ee6890b49661cfaf3
Maximilian Erfurt <maximilian.erfurt@gmail.com>
2023-06-15 16:50:20 +0200

    Changes Maxi

--- Kalibriermatrix.py
+++ Kalibriermatrix.py
@@ -7,10 +7,6 @@ import tkinter
 from tkinter import filedialog
 from skimage.morphology import skeletonize
 
-# global var
-pixel_cords = np.ones((3, 4))
-real_cords = np.ones((4, 4))
-xi, yi = 0, 0
 
 # function to get the pixel cords
 # on mouseclick
@@ -23,6 +19,11 @@ def click_event(event, x, y, flags, params):
         yi = y
 
 
+# global var
+pixel_cords = np.ones((3, 4))
+real_cords = np.ones((4, 4))
+test_cord = np.ones((3, 1))
+xi, yi = 0, 0
 # reading the image
 img = cv2.imread('KalibrierDraht.jpeg', 1)
 
@@ -40,18 +41,31 @@ for i in range(4):
 print(pixel_cords)
 print(np.linalg.pinv(pixel_cords))
 
-
 # ask for real cords
-for i in range(4):
-    print("Point" + str(i+1) + " x :")
-    real_cords[0, i] = input()
-    print("Point" + str(i + 1) + " y :")
-    real_cords[1, i] = input()
-    print("Point" + str(i + 1) + " z :")
-    real_cords[2, i] = input()
+# for i in range(4):
+#     print("Point" + str(i + 1) + " x :")
+#     real_cords[0, i] = input()
+#     print("Point" + str(i + 1) + " y :")
+#     real_cords[1, i] = input()
+#     print("Point" + str(i + 1) + " z :")
+#     real_cords[2, i] = input()
+real_cords = np.array([[112.06, 210.75, 456.48, 629.8],
+                       [663.58, 647.66, 621.83, 765.42],
+                       [-27.28, -19.77, -38.8, -46.59]])
+print(real_cords)
+
 inv_pixel_cords = np.linalg.pinv(pixel_cords)
 k = real_cords.dot(inv_pixel_cords)
-print(k)
+print("Kalibriermatrix",k)
 
-
-
+# verify
+cv2.imshow('image', img)
+cv2.setMouseCallback('image', click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+test_cord[0, 0] = xi
+test_cord[1, 0] = yi
+test_cord[2, 0] = 1
+print(test_cord)
+p = k.dot(test_cord)
+print(p)
